d7d82dbd2016d833a28869acd6988143487a786f
Naga <nagapraveenaga348@gmail.com>
2024-09-10 18:59:30 -0400

    Completed the Add List-Item Selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 
 import './App.css'
+import {  useState } from 'react';
 
 function App() {
+  let data={
+    name:"Jack Jackson",
+    email:"jack@abc.com",
+    password:"jackj"
+  }
+  const [selectedEntry,setSelectedEntry]=useState(data);
+ 
+
+ 
+
 function onDelete(){
   console.log("Clicked delete button");
 }
@@ -11,8 +22,51 @@ function onSave(){
 function onCancel(){
   console.log("Clicked on Save button");
 }
-function handleList(){
-  console.log("Clicked on the entry in the list!!");
+function handleList(e){
+  
+ var id=e.target.parentNode.id;
+console.log("Clicked on the entry in the list!!");
+
+console.log(id);
+var element=document.getElementById(id);
+console.log(element)
+let name=element.cells[0]['innerText']
+console.log(name)
+let email=element.cells[1]['innerText']
+console.log(email)
+let password=element.cells[2]['innerText']
+console.log(password)
+let newdata={
+  name:name,
+  email:email,
+  password:password
+}
+console.log(newdata.email)
+setSelectedEntry(newdata)
+
+console.log("After set method: ")
+console.log(selectedEntry)
+}
+
+function handleChange(e){
+  console.log(e.target.id)
+  let name,email,password;
+  if(e.target.id==='name'){
+    name=e.target.value;
+    setSelectedEntry(prev=>({...prev,name:name}))
+  }
+  
+else if(e.target.id==='email'){
+  email=e.target.value;
+  setSelectedEntry(prev=>({...prev,email:email}))
+}
+
+else{
+  password=e.target.value;
+  setSelectedEntry(prev=>({...prev,password:password}))
+}
+
+  
 }
 
   return (
@@ -30,7 +84,7 @@ function handleList(){
         </thead>
         <tbody>
           
-        <tr onClick={handleList} style={{cursor:"pointer"}}>
+        <tr id="entry1" onClick={(e)=>handleList(e)} style={{cursor:"pointer"}}>
           
         <td>Jack Jackson</td>
         <td>jack@abc.com</td>
@@ -38,14 +92,14 @@ function handleList(){
         </tr>
         
        
-        <tr onClick={handleList} style={{cursor:"pointer"}}>
+        <tr id="entry2" onClick={(e)=>handleList(e)} style={{cursor:"pointer"}}>
         <td>Katie Kates</td>
         <td>katiek@abc.com</td>
         <td>katiek</td>
         </tr>
         
         
-        <tr onClick={handleList} style={{cursor:"pointer"}}>
+        <tr id="entry3" onClick={(e)=>handleList(e)} style={{cursor:"pointer"}}>
         <td>Glen Glenns</td>
         <td>gleng@abc.com</td>
         <td>gleng</td>
@@ -57,13 +111,13 @@ function handleList(){
       <h3>Update</h3>
       <form className='form-style'>
         <label name="name" htmlFor="name" >Name: </label><br/>
-        <input type="text" id="name" placeholder='enter your name' required />
+        <input type="text" id="name" placeholder='enter your name' onChange={(e)=>handleChange(e)} value={selectedEntry.name}  />
         <br/>
         <label name="email" htmlFor="email" >Email: </label><br/>
-        <input type="email" id="email" placeholder='enter your email' required/>
+        <input type="email" id="email" placeholder='enter your email' value={selectedEntry.email} onChange={(e)=>handleChange(e)} required/>
         <br/>
         <label htmlFor="password" >Pass: </label><br/>
-        <input name="password" id="password" placeholder='enter your password' required/>
+        <input name="password" id="password" placeholder='enter your password'value={selectedEntry.password}  onChange={(e)=>handleChange(e)} required/>
         <br/>
         <button onClick={onDelete} id="delete">Delete</button>
         <button onClick={onSave} id="save">Save</button>
